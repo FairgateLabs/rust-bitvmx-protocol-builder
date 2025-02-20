6948790dc40881a2e7e1e09f23b879ecf321fc06
Pedro Prete <pmprete@Pedros-MacBook-Pro.local>
2025-02-20 16:53:53 -0300

    Add taproot_keypath tests

--- src/tests/builder_test/builder_outputs_test.rs
+++ src/tests/builder_test/builder_outputs_test.rs
@@ -1,21 +1,18 @@
+
 #[cfg(test)]
 mod tests {
     use bitcoin::{
-        hashes::Hash,
-        key::rand::RngCore,
-        opcodes::all::OP_RETURN,
-        secp256k1::{self},
-        Amount, EcdsaSighashType, PublicKey, ScriptBuf, TapSighashType, XOnlyPublicKey,
+        hashes::Hash, key::rand::RngCore, opcodes::all::{OP_PUSHNUM_1, OP_RETURN}, secp256k1::{self}, Amount, PublicKey, ScriptBuf, Sequence
     };
     use std::{env, path::PathBuf, rc::Rc};
     use storage_backend::storage::Storage;
 
     use crate::{
-        builder::{ProtocolBuilder, SpendingArgs},
+        builder::ProtocolBuilder,
         errors::ProtocolBuilderError,
-        graph::{input::SighashType, output::OutputSpendingType},
-        scripts::ProtocolScript,
-        unspendable::unspendable_key,
+        graph::output::OutputSpendingType,
+        scripts::ProtocolScript, 
+        tests::common,
     };
     fn temp_storage() -> PathBuf {
         let dir = env::temp_dir();
@@ -27,7 +24,7 @@ mod tests {
     
     #[test]
     fn test_op_return_output_script() -> Result<(), ProtocolBuilderError> {
-        let ecdsa_sighash_type = SighashType::Ecdsa(EcdsaSighashType::All);
+        let ecdsa_sighash_type = common::ecdsa_sighash_type();
         let value = 1000;
         let txid = Hash::all_zeros();
         let output_index = 0;
@@ -94,6 +91,86 @@ mod tests {
             "Second instruction should be data we sent"
         );
 
+        Ok(())
+    }
+
+    #[test]
+    fn test_taproot_keypath_and_speedup() -> Result<(), anyhow::Error> {
+        // Arrange
+        let key_manager = common::new_key_manager()?;
+        let ecdsa_sighash_type = common::ecdsa_sighash_type();
+        let taproot_sighash_type = common::taproot_sighash_type();
+        let value = 1000;
+        let txid = Hash::all_zeros();
+        let output_index = 0;
+        let public_key = key_manager.derive_keypair(0).unwrap();
+        let script = ProtocolScript::new(ScriptBuf::from(vec![0x04]), &public_key);
+        let output_spending_type =
+            OutputSpendingType::new_segwit_script_spend(&script, Amount::from_sat(value));
+    
+        let speedup_value = 2450000;
+        let pubkey_alice = key_manager.derive_keypair(1).unwrap();
+    
+        // Act
+        let storage = Rc::new(Storage::new_with_path(&temp_storage())?);
+        let mut builder = ProtocolBuilder::new("tap_keypath", storage.clone())?;
+        let protocol =builder.connect_with_external_transaction(
+            txid,
+            output_index,
+            output_spending_type,
+            "keypath_origin",
+            &ecdsa_sighash_type,
+        )?
+        .add_taproot_key_spend_connection(
+            "connection",
+            "keypath_origin",
+            value,
+            &public_key,
+            "keypath_spend",
+            &taproot_sighash_type,
+        )?
+        .add_speedup_output("keypath_origin", speedup_value, &pubkey_alice)?
+        .add_p2wpkh_output(
+            "keypath_spend",
+            value,
+            &pubkey_alice
+        )?
+        .build_and_sign(&key_manager)?;
+
+        let tx_origin = protocol.transaction("keypath_origin")?;
+        let tx_spend = protocol.transaction("keypath_spend")?;
+        //println!("tx: {:?}", tx_spended);
+        // Assert
+        assert_eq!(tx_origin.output.len(), 2, "Origin transaction should have 2 outputs");
+        let script_taproot_output = tx_origin.output[0].script_pubkey.clone();
+        //println!("script_taproot_output: {:?}", hex::encode(script_taproot_output.to_bytes()));
+        let taproot_output_instructions = script_taproot_output
+            .instructions()
+            .flatten()
+            .collect::<Vec<_>>();
+        // Check Origin Outputs
+        assert_eq!(tx_origin.output.len(), 2, "Origin transaction should have 2 outputs");
+        assert_eq!(tx_origin.output[0].value, Amount::from_sat(value), "Origin output should have the value we sent");
+        assert_eq!(tx_origin.output[1].value, Amount::from_sat(speedup_value), "Origin output should have the speedup value");
+        assert_eq!(taproot_output_instructions.len(), 2, "Taproot output script should have 2 instructions");
+        assert_eq!(
+            taproot_output_instructions[0].opcode(),
+            Some(OP_PUSHNUM_1),
+            "First taproot output instruction should be OP_1"
+        );
+        assert_eq!(
+            taproot_output_instructions[1].push_bytes().unwrap().len(),
+            32,
+            "Second taproot output instruction should be OP_PUSHBYTES_32 with the hashed tweaked pubkey"
+        );
+
+        // Check Spend Input
+        println!("tx_spend.input: {:?}", tx_spend.input);
+        assert_eq!(tx_spend.input.len(), 1, "Spend transaction should have 1 input");
+        assert_eq!(tx_spend.input[0].previous_output.txid, tx_origin.compute_txid(), "Spend input should have the same txid as the origin transaction");
+        assert_eq!(tx_spend.input[0].previous_output.vout, 0, "Spend input should have the same output index as the origin transaction");
+        assert_eq!(tx_spend.input[0].sequence, Sequence::from_hex("0xfffffffd").unwrap(), "Spend input should have the sequence number 0xfffffffd");
+        
         Ok(())
     }
 
