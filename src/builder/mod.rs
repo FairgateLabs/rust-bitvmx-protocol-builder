208f2e7f78d1710403ae0ace8b6fd0bfa8c93cc2
Pedro Prete <pmprete@Pedros-MacBook-Pro.local>
2025-02-26 16:45:44 -0300

    Add SpendingArgs to protocol.rs file

--- src/builder/mod.rs
+++ src/builder/mod.rs
@@ -1,5 +1,4 @@
 mod builder;
 mod protocol;
-mod spending_args;
 
-pub use self::{builder::ProtocolBuilder, spending_args::SpendingArgs};
+pub use self::{builder::ProtocolBuilder, protocol::SpendingArgs};
