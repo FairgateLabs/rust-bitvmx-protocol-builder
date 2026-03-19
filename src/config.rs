c3a943ecdf26801a330755e3944422bcc0039c7f
Tomas Szejnfeld Sirkis <86393038+TomasSzeSirk@users.noreply.github.com>
2026-03-19 09:34:18 -0300

    Replace settings to use bitvmx-settings lib (#86)
    * Add bitvmx-settings dependency and update configuration handling
    
    * Replace .json config with .yaml config
    
    * Refactor configuration loading per comments in PR
    
    ---------
    
    Co-authored-by: Martin Jonas <martinjonas@gmail.com>

--- src/config.rs
+++ src/config.rs
@@ -1,15 +1,9 @@
 use bitvmx_bitcoin_rpc::rpc_config::RpcConfig;
-use config as settings;
 use key_manager::config::KeyManagerConfig;
 use serde::Deserialize;
-use std::env;
 use storage_backend::storage_config::StorageConfig;
-use tracing::warn;
 
 use crate::errors::ConfigError;
-
-static DEFAULT_ENV: &str = "development";
-static CONFIG_PATH: &str = "config";
 
 #[derive(Debug, Deserialize)]
 pub struct ProtocolBuilderConfig {
@@ -31,32 +25,7 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new() -> Result<Config, ConfigError> {
-        let env = Config::get_env();
-        Config::parse_config(env)
-    }
-
-    fn get_env() -> String {
-        env::var("BITVMX_ENV").unwrap_or_else(|_| {
-            let default_env = DEFAULT_ENV.to_string();
-            warn!(
-                "BITVMX_ENV not set. Using default environment: {}",
-                default_env
-            );
-            default_env
-        })
-    }
-
-    fn parse_config(env: String) -> Result<Config, ConfigError> {
-        let config_path = format!("{}/{}.json", CONFIG_PATH, env);
-
-        let settings = settings::Config::builder()
-            .add_source(config::File::with_name(&config_path))
-            .build()
-            .map_err(ConfigError::ConfigFileError)?;
-
-        settings
-            .try_deserialize::<Config>()
-            .map_err(ConfigError::ConfigFileError)
+    pub fn new(config: Option<String>) -> Result<Config, ConfigError> {
+        Ok(bitvmx_settings::settings::load_config_file::<Config>(config)?)
     }
 }
